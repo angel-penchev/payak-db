cdd548142de9aea243b1588faad083ec8d0d84a6
Angel Penchev <angel@penchev.dev>
2026-02-10 00:27:50 +0200

    fix: delimiters in project url matching

--- index.php
+++ index.php
@@ -45,18 +45,19 @@ switch ($request) {
             break;
         }
 
-        // Route: Create Project (GET /courses/w25-2025-2026/project-create)
-        if (preg_match('/^courses\/([a-zA-Z0-9\-_]+)$/project-create$/', $request, $matches)) {
-            $courseId = $matches[1];
-            $view = 'views/project_create.php';
-            break;
-        }
 
         // Route: View Project (GET /courses/w25-2025-2026/projects/fcf9edf9-0d01-4250-b17b-a6e3c466a7df)
-        if (preg_match('/^courses\/([a-zA-Z0-9\-_]+)$/projects\/([a-zA-Z0-9\-_]+)$/', $request, $matches)) {
+        if (preg_match('#^courses\/([a-zA-Z0-9\-_]+)/projects\/([a-zA-Z0-9\-_]+)$#', $request, $matches)) {
             $courseId = $matches[1];
             $projectId = $matches[2];
             $view = 'views/project_detail.php';
+            break;
+        }
+
+        // Route: Create Project (GET /courses/w25-2025-2026/project-create)
+        if (preg_match('#^courses/([a-zA-Z0-9\-_]+)/project-create$#', $request, $matches)) {
+            $courseId = $matches[1];
+            $view = 'views/project_create.php';
             break;
         }
 
